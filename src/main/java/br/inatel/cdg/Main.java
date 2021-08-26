1cef708958997bcf956b3fe01e3a7ed884958a0a
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2021-08-25 21:15:09 -0300

    create main class

---
+++ src/main/java/br/inatel/cdg/Main.java
@@ -0,0 +1,9 @@
+package br.inatel.cdg;
+
+import br.inatel.cdg.utils.CsvUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        CsvUtils.readAndParseCsvToGameList();
+    }
+}
